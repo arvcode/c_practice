1bb43b23181eafc3f0201aa75b0fba68b5b264d4
Arvindh <srinivasan.arvindh@sg.panasonic.com>
2018-05-11 16:42:53 +0800

    character

---
+++ char.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<string.h>
+main() {
+	int a;
+	int b;
+	a=10;
+	b=10;
+	scanf("%d %d",&a,&b);
+ 	char c[a+b+1];
+	memset(c,'1',a+b);
+	c[a+b]='\0';
+	printf("strlen is %d \n",strlen(c));
+	printf("%s",c);
+
+}
